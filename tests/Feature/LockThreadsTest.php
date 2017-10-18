f5567e01f5c0beca3e332ad7a1680fae0115abcb
Francis Morissette <Francismori7@users.noreply.github.com>
2017-10-17 21:39:48 -0400

    Fix test for unlocking threads
    Thread was unlocked instead of locked when using the migration.

--- tests/Feature/LockThreadsTest.php
+++ tests/Feature/LockThreadsTest.php
@@ -40,7 +40,7 @@ class LockThreadsTest extends TestCase
     {
         $this->signIn(factory('App\User')->states('administrator')->create());
 
-        $thread = create('App\Thread', ['user_id' => auth()->id(), 'locked' => false]);
+        $thread = create('App\Thread', ['user_id' => auth()->id(), 'locked' => true]);
 
         $this->delete(route('locked-threads.destroy', $thread));
 
