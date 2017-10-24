6268dce90b16a5da19f97480f250aec1f4e6031f
Jeffrey Way <jeffrey@jeffrey-way.com>
2017-10-24 11:29:45 -0400

    Episode 88

--- app/Filters/Filters.php
+++ app/Filters/Filters.php
@@ -61,6 +61,6 @@ abstract class Filters
      */
     public function getFilters()
     {
-        return $this->request->intersect($this->filters);
+        return array_filter($this->request->only($this->filters));
     }
 }
--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -24,7 +24,7 @@ class RegistrationTest extends TestCase
             'password_confirmation' => 'foobar'
         ]);
 
-        Mail::assertSent(PleaseConfirmYourEmail::class);
+        Mail::assertQueued(PleaseConfirmYourEmail::class);
     }
 
     /** @test */
